02db244f5179c5b5c1b4d3e10fe119714ffd7b08
anupamgodse <anupamgodse121@gmail.com>
2015-10-24 21:12:25 +0530

    Create 7.h

---
+++ 7.h
@@ -0,0 +1,31 @@
+typedef struct dirent dirent;
+typedef struct node {
+	char *mypath;
+	off_t fsize;
+	struct node *dup, *dupsign;
+	struct node *nnext, *nprev;
+	int visited;
+}node;
+typedef struct mydir {
+	node *nhead, *ntail;
+	struct mydir *mnext, *mprev;
+	char *myname;
+}mydir;
+typedef struct myplan {
+	mydir *head, *tail;
+	int count;
+}myplan;
+typedef struct flink {
+	off_t size;
+	node *tail;
+}flink;
+void sortdir(myplan *, char *);
+void appendmydir(mydir *, node *);
+void appendmyplan(myplan *, mydir *);
+void printmyplan(myplan *);
+off_t filesize(char *filename);
+void linkbysize(myplan *mp);
+void linkbyhashing(myplan *mp);
+void finallink(myplan *mp);
+int comparefiles(char *file1, char *file2);
+char *getsign(char *fname);
