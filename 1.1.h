5089af08cb1c031226fb670b5cb03dbd8c0f5cfb
anupamgodse <anupamgodse121@gmail.com>
2015-10-24 21:06:04 +0530

    Create 1.1.h

---
+++ 1.1.h
@@ -0,0 +1,18 @@
+typedef struct dirent dirent;
+typedef struct node {
+	dirent *cdir;
+	struct node *nnext, *nprev;
+}node;
+typedef struct mydir {
+	node *nhead, *ntail;
+	struct mydir *mnext, *mprev;
+}mydir;
+typedef myplan {
+	mydir *head, *tail;
+}myplan;
+typedef struct stat stat;
+void sortdir(mydir *, name);
+void initmyplan(myplan *);
+void initmydir(mydir *);
+void appendmydir(mydir *, node *);
+void appendmyplan(myplan *, mydir *);
