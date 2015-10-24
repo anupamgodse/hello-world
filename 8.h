6cc07935231dad79d29b931121f004a289aa3e46
anupamgodse <anupamgodse121@gmail.com>
2015-10-24 21:17:04 +0530

    Create 8.h

---
+++ 8.h
@@ -0,0 +1,37 @@
+typedef struct dirent dirent;
+static struct option long_options[] = {
+	{ "omitfirst", 0, 0, 'f' },
+    	{ "recurse", 0, 0, 'r' },
+    	{ "delete", 0, 0, 'd' },
+    	{ 0, 0, 0, 0 }
+};
+typedef struct node {
+	char *mypath;
+	off_t fsize;
+	struct node *dup, *dupsign;
+	struct node *nnext, *nprev;
+	int visited;
+}node;
+typedef struct mydir {
+	node *nhead, *ntail;
+	struct mydir *mnext, *mprev;
+	char *myname;
+}mydir;
+typedef struct myplan {
+	mydir *head, *tail;
+	int count;
+}myplan;
+typedef struct flink {
+	off_t size;
+	node *tail;
+}flink;
+void sortdir(myplan *, char *);
+void appendmydir(mydir *, node *);
+void appendmyplan(myplan *, mydir *);
+void printmyplan(myplan *);
+off_t filesize(char *filename);
+void linkbysize(myplan *mp);
+void linkbyhashing(myplan *mp);
+void finallink(myplan *mp);
+int comparefiles(char *file1, char *file2);
+char *getsign(char *fname);
